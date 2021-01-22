87765ffc47a3d726bc4cd7faef9a87c5692cd783
Joilence <cjyk84719@gmail.com>
2021-01-22 11:13:41 +0100

    add: load fma metadata

--- utils.py
+++ utils.py
@@ -1,4 +1,56 @@
 from pathlib import Path
+import pandas as pd
+import os
+import ast
 
 def get_all_audio_paths(path):
     return [str(e) for e in list(Path(path).rglob("*.mp3"))]
+
+
+def fma_load(filepath):
+    """
+    Load files provided by fma with their specification, taken from mdeff/fma.
+    """    
+
+    filename = os.path.basename(filepath)
+
+    if 'features' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'echonest' in filename:
+        return pd.read_csv(filepath, index_col=0, header=[0, 1, 2])
+
+    if 'genres' in filename:
+        return pd.read_csv(filepath, index_col=0)
+
+    if 'tracks' in filename:
+        tracks = pd.read_csv(filepath, index_col=0, header=[0, 1])
+
+        COLUMNS = [('track', 'tags'), ('album', 'tags'), ('artist', 'tags'),
+                   ('track', 'genres'), ('track', 'genres_all')]
+        for column in COLUMNS:
+            tracks[column] = tracks[column].map(ast.literal_eval)
+
+        COLUMNS = [('track', 'date_created'), ('track', 'date_recorded'),
+                   ('album', 'date_created'), ('album', 'date_released'),
+                   ('artist', 'date_created'), ('artist', 'active_year_begin'),
+                   ('artist', 'active_year_end')]
+        for column in COLUMNS:
+            tracks[column] = pd.to_datetime(tracks[column])
+
+        SUBSETS = ('small', 'medium', 'large')
+        try:
+            tracks['set', 'subset'] = tracks['set', 'subset'].astype(
+                    'category', categories=SUBSETS, ordered=True)
+        except ValueError:
+            # the categories and ordered arguments were removed in pandas 0.25
+            tracks['set', 'subset'] = tracks['set', 'subset'].astype(
+                     pd.CategoricalDtype(categories=SUBSETS, ordered=True))
+
+        COLUMNS = [('track', 'genre_top'), ('track', 'license'),
+                   ('album', 'type'), ('album', 'information'),
+                   ('artist', 'bio')]
+        for column in COLUMNS:
+            tracks[column] = tracks[column].astype('category')
+
+        return tracks
