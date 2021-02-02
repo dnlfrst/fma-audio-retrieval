967405839a973434b827cc5110770f600cf928c8
Daniel Fürst <dpfuerst@gmail.com>
2021-02-02 21:43:16 +0100

    improvement(track-viewer): display genre and rating

--- src/frontend/src/TrackViewer.tsx
+++ src/frontend/src/TrackViewer.tsx
@@ -1,10 +1,35 @@
 import { LoadingOutlined, SwapOutlined } from "@ant-design/icons";
-import { Button, Space, Spin, Typography } from "antd";
+import { Button, Rate, Space, Spin, Tag, Typography } from "antd";
+import { scaleLinear, scaleLog } from "d3-scale";
 import React from "react";
+import styled from "styled-components";
+import getColorForGenre from "./colors";
+import { Genre } from "./TrackGenrePrediction";
 import useTracks from "./useTracks";
 import Widget from "./Widget";
 
 const { Text, Title } = Typography;
+
+const AttributedTitle = styled(Title)`
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CompactRating = styled(Rate)`
+  margin: 0 !important;
+
+  li {
+    margin: 0 !important;
+    padding: 0 !important;
+    top: -1px;
+  }
+`;
+
+const FlexibleContainer = styled.div`
+  align-items: center;
+  display: flex;
+`;
 
 const TrackViewer = ({
   selectRandomTrack,
@@ -14,6 +39,15 @@ const TrackViewer = ({
   trackID: string;
 }) => {
   const tracks = useTracks();
+  const minimumPopularity = Math.min(
+    ...tracks.map((track) => track.popularity)
+  );
+  const maximumPopularity = Math.max(
+    ...tracks.map((track) => track.popularity)
+  );
+  const popularityScale = scaleLog()
+    .domain([minimumPopularity, maximumPopularity])
+    .range([0, 5]);
 
   const selectedTrack = tracks?.find((track) => track.ID === trackID);
   const width = 450;
@@ -36,9 +70,26 @@ const TrackViewer = ({
     >
       {selectedTrack ? (
         <>
-          <Title ellipsis level={3}>
-            {selectedTrack.title}
-          </Title>
+          <AttributedTitle level={3}>
+            <Text ellipsis style={{ maxWidth: widthWithoutPadding / 2 }}>
+              {selectedTrack.title}
+            </Text>
+            <FlexibleContainer>
+              {selectedTrack.genre ? (
+                <Tag
+                  color={getColorForGenre(selectedTrack.genre as Genre).fill}
+                >
+                  {selectedTrack.genre}
+                </Tag>
+              ) : null}
+              {selectedTrack.popularity !== undefined ? (
+                <CompactRating
+                  disabled
+                  value={popularityScale(selectedTrack.popularity)}
+                />
+              ) : null}
+            </FlexibleContainer>
+          </AttributedTitle>
           <Text
             ellipsis
             style={{ maxWidth: widthWithoutPadding }}
